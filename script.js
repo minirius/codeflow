eae6b1b542de23771da44c939f699763755896af
minirius <101399739+minirius@users.noreply.github.com>
2024-03-23 18:52:04 +0100

    j1

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function playPauseVideo() {
+    let videos = document.querySelectorAll("video");
+    videos.forEach((video) => {
+        // We can only control playback without insteraction if video is mute
+        video.muted = true;
+        // Play is a promise so we need to check we have it
+        let playPromise = video.play();
+        if (playPromise !== undefined) {
+            playPromise.then((_) => {
+                let observer = new IntersectionObserver(
+                    (entries) => {
+                        entries.forEach((entry) => {
+                            if (
+                                entry.intersectionRatio !== 1 &&
+                                !video.paused
+                            ) {
+                                video.pause();
+                            } else if (video.paused) {
+                                video.play();
+                            }
+                        });
+                    },
+                    { threshold: 0.2 }
+                );
+                observer.observe(video);
+            });
+        }
+    });
+}
+
 async function main() {
     const response = await fetch("https://raw.githubusercontent.com/minirius/codeflow/main/videos/videos.json");
     movies = await response.json();
@@ -21,6 +51,9 @@ async function main() {
         </div>
         `;
     });
+
+    // And you would kick this off where appropriate with:
+    playPauseVideo();
 }
 
 window.addEventListener('DOMContentLoaded', main());
